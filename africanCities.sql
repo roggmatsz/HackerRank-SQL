89006044910ab4a171c718d2a9aced40995abe91
Rogger Matamoros <roggmatz@gmail.com>
2017-01-10 11:39:10 -0500

    Attempt 1.

--- africanCities.sql
+++ africanCities.sql
@@ -1,3 +1,7 @@
 -- Given tables 'City' and 'Country', query all cities in 
 -- Africa
 
+SELECT city.name
+FROM city INNER JOIN country 
+ON city.CountryCode = country.code
+WHERE country.continent = 'Africa'
