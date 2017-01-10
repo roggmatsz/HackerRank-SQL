c123bb615ccca91a12cc12d65734bc567c6e5758
Rogger Matamoros <roggmatz@gmail.com>
2017-01-10 12:47:53 -0500

    Attempt 2.

--- avgContinentPop.sql
+++ avgContinentPop.sql
@@ -1,6 +1,9 @@
 -- Given tables 'City' and 'Country', query all continent names
 -- and their respective average city populations.
 
-SELECT country.continent, avg(city.population)
+SELECT country.continent, 
+    (SELECT avg(city.population)
+     FROM city
+     WHERE country.code = city.countryCode)
 FROM country INNER JOIN city
 ON country.code = city.countryCode 
