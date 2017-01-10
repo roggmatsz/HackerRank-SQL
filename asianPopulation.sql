d54f02a3ec4566f76b41870556bb8986544f41b7
Rogger Matamoros <roggmatz@gmail.com>
2017-01-10 11:26:52 -0500

    Inital commit, problem definition.

---
+++ asianPopulation.sql
@@ -0,0 +1,5 @@
+-- Given two tables, one called 'Country' and the other 'City'
+-- with columns of equal values called 'CountryCode' and 'code' 
+-- respectively, query the sum of the populations of all cities
+-- in Asia.
+
