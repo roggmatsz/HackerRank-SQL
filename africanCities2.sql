f0f7dd8e7b3a187a62783cc29384f30e048abf1b
Rogger Matamoros <roggmatz@gmail.com>
2017-01-10 11:45:17 -0500

    Attempt 1.

--- africanCities2.sql
+++ africanCities2.sql
@@ -1,3 +1,7 @@
 -- Given tables city and country, query the names of all cities
 -- in Africa.
 
+SELECT city.name
+FROM city INNER JOIN country
+ON city.countryCode = country.code
+WHERE country.continent = 'Africa'
