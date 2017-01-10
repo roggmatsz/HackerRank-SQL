eade92f612739b413ef482e9684814564154be26
Rogger Matamoros <roggmatz@gmail.com>
2017-01-10 11:34:28 -0500

    Attempt 1.

--- asianPopulation.sql
+++ asianPopulation.sql
@@ -3,3 +3,7 @@
 -- respectively, query the sum of the populations of all cities
 -- in Asia.
 
+SELECT sum(city.population)
+FROM city INNER JOIN Country
+ON city.CountryCode = country.code
+WHERE country.continent = 'Asia'
